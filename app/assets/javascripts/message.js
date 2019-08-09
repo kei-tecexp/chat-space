28f62be6d36ecc970002a689d78e91376dc71d97
kei_tecexp <keitassssm@gmail.com>
2019-08-09 20:34:29 +0900

    edit message send

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,81 @@
+$(function() {
+
+  function buildHTML(message){
+    if (message.image_url && message.body) {
+      var html = `<div class="message">
+                    <div class="upper-message">
+                      <div class="upper-message__user-name">
+                        ${message.user_name}
+                      </div>
+                      <div class="upper-message__date">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="lower-meesage">
+                      <div class="lower-message__content">
+                        ${message.body}
+                      </div>
+                      <img class="lower-message__image" src="${message.image_url}" alt="">
+                    </div>
+                  </div>`
+    } else if (message.body) {
+      var html = `<div class="message">
+                    <div class="upper-message">
+                      <div class="upper-message__user-name">
+                        ${message.user_name}
+                      </div>
+                      <div class="upper-message__date">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="lower-meesage">
+                      <div class="lower-message__content">
+                        ${message.body}
+                      </div>
+                    </div>
+                  </div>`
+      
+    } else {
+      var html = `<div class="message">
+                    <div class="upper-message">
+                      <div class="upper-message__user-name">
+                        ${message.user_name}
+                      </div>
+                      <div class="upper-message__date">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="lower-meesage">
+                      <img class="lower-message__image" src="${message.image_url}" alt="">
+                    </div>
+                  </div>`
+    } 
+    return html;
+  }
+
+  $('#new_message').on('submit', function(e) {
+    e.preventDefault();
+    var formData = new FormData(this);
+    var href = $(this).attr('action');
+    $.ajax({
+      url: href,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.form__message').val('');
+      $('.hidden').val('');
+      $('.form__submit').removeAttr('disabled');
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+    })
+    .fail(function(){
+      alert('メッセージを入力してください。');
+      $('.form__submit').removeAttr('disabled');
+    })
+  });
+});
